e30f99ffea8a1ea7f8def507c106147e783b36c4
Achilles Moraites <achimoraites@yahoo.gr>
2019-05-12 20:13:13 +0300

    added category filtering

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PRODUCTS } from '../model/mock-products';
+import { Product } from '../model/product';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,18 +9,32 @@ import { PRODUCTS } from '../model/mock-products';
 })
 export class ProductsComponent implements OnInit {
 
-  products = PRODUCTS;
+  allProducts = PRODUCTS;
   categories = ['Books', 'Programming', 'Random'];
+  selectedCategory = null;
+
   constructor() { }
 
   getCategories() {
     const categoriesFromProducts =
-    this.products.map(product => product.categories)
-    .reduce((acc, cur) => acc.concat(cur));
+      this.products.map(product => product.categories)
+        .reduce((acc, cur) => acc.concat(cur));
     const uniqueCategories = Array.from(new Set(categoriesFromProducts).values());
     this.categories = uniqueCategories;
     console.log(uniqueCategories);
   }
+
+  get products(): Product[] {
+    if(this.selectedCategory == null) {
+      return this.allProducts;
+    }
+    return this.allProducts.filter(product => product.categories.includes(this.selectedCategory));
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
   ngOnInit() {
     this.getCategories();
   }
